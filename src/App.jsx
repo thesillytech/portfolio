e36171358002756e989561c1e6ba01e1ef21af1f
BoBo <sanilpsy@gmail.com>
2025-02-05 17:44:20 -0600

    adding app file

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,9 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./Home.jsx"
-import About from "./About.jsx"
 
 
 export default function App () {
-    
+    <></>
 }
 
 
