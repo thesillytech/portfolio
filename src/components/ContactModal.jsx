ad967bc3e410543da031ea15683e0c03308e4317
BoBo <sanilpsy@gmail.com>
2025-02-27 21:13:59 -0600

    thinking about to use modal or not

--- src/components/ContactModal.jsx
+++ src/components/ContactModal.jsx
@@ -12,7 +12,16 @@ function ContactModal ({ isOpen, isClose}) {
                 onClose={isClose}
             >
                 <Box sx={css.Modal}>
-                    <Box>Name: <TextField id="filled-basic" label="Name" variant="outlined" size="small" sx={css.TextBox} /></Box>
+                    <Box>
+                        {/* <Box sx={css.Title}>Name: </Box> */}
+                        <TextField id="filled-basic" label="Name" variant="outlined" size="small" sx={css.TextBox} />
+                    </Box>
+                    <Box>
+                        <TextField id="filled-basic" label="Email" variant="outlined" size="small" sx={css.TextBox} />
+                    </Box>
+                    <Box>
+                        <TextField id="filled-basic" label="Message" variant="outlined" size="small" sx={css.TextBox} />
+                    </Box>
                     <Button variant="outlined" onClick={isClose} sx={css.CloseButton}>Close</Button>
                 </Box>
             </Modal>
@@ -23,6 +32,13 @@ function ContactModal ({ isOpen, isClose}) {
 export default ContactModal
 
 const css = {
+    Title: {
+        display: 'flex',
+        alignItems: 'center',
+        marginRight: '10px',
+        color: '#fff'
+    },
+
     Modal: {
         position: 'absolute',
         top: '50%',
@@ -37,13 +53,14 @@ const css = {
         display: 'flex',
         flexDirection: 'column',
         alignItems: 'center',
-        justifyContent: 'center'
+        '@media screen and (max-width: 600px)': {
+            width: '200px',
+        }
     }, 
 
     CloseButton: {
         color: '#c3c39a',
         border: '1px solid #c3c39a',
-        marginTop: '16px',
         '& .MuiButtonBase-root': {
             textTransform: 'initial'
         },
@@ -57,6 +74,7 @@ const css = {
     }, 
 
     TextBox: {
+        marginBottom: '20px',
         '& .MuiFormLabel-root': {
             color: '#c3c39a',   // Text color of placeholder label within TextField
         },
