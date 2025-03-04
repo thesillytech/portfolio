dadb7c2b8e01faa1b50c41baf94512f48d57a865
BoBo <sanilpsy@gmail.com>
2025-03-03 20:25:56 -0600

    fixed image url

--- src/Projects.jsx
+++ src/Projects.jsx
@@ -17,7 +17,7 @@ function Projects () {
                     {/* Portfolio */}
                     <Grid container spacing={3} sx={{ marginBottom: '10px' }}>
                         <Grid>
-                            <img src="/src/assets/portfolio.png" style={css.Img}></img>
+                            <img src="./src/assets/portfolio.png" style={css.Img}></img>
                         </Grid>
                         <Grid>
                             <Box sx={{ marginTop: '-10px', color: '#c3c39a'}}>Portfolio</Box>
